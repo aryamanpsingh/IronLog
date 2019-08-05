15003f28305019ec3bfe4990a16fd5ff883abe5b
Aryaman Singh <aps010@ucsd.edu>
2019-08-05 02:11:17 -0700

    changed db name

--- IronLog/settings.py
+++ IronLog/settings.py
@@ -92,7 +92,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ironlog',
+            'NAME': 'ironlogdb',
             'USER': 'arya',
             'PASSWORD': 'postgresiron',
             'HOST': 'localhost',
