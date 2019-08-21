2787b9d56650e662e54bdf9bf27a75b184eb9dac
Aryaman Singh <aps010@ucsd.edu>
2019-08-21 12:14:13 -0700

    changed routine privacy

--- frontend/src/actions/routine.js
+++ frontend/src/actions/routine.js
@@ -1,31 +1,39 @@
 import axios from "axios";
 import { ADD_ROUTINE } from "./types";
 import { GET_ROUTINES } from "./types";
+import { tokenConfig } from "./users";
+import { createMessage, returnErrors } from "./messages";
 
 // ADD Routine
-export const addRoutine = routine => dispatch => {
+export const addRoutine = routine => (dispatch, getState) => {
   // Headers
   axios
-    .post("/api/create/routine/", routine)
+    .post("/api/create/routine/", routine, tokenConfig(getState))
+
     .then(res => {
+      dispatch(createMessage({ addRoutine: "Routine Added" }));
       dispatch({
         type: ADD_ROUTINE,
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 // List Routine
-export const getRoutine = () => dispatch => {
+export const getRoutine = () => (dispatch, getState) => {
   // Headers
   axios
-    .get("/api/routine")
+    .get("/api/routine", tokenConfig(getState))
     .then(res => {
       dispatch({
         type: GET_ROUTINES,
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
